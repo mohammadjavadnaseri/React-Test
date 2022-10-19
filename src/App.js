d83608c620b6038a7c1cd26423742d99ec00eb45
mohammadjavadnaseri <mjavadnaseri82@gmail.com>
2022-10-20 00:11:49 +0330

    React Test File

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='ValedDiv'>
+      <p> This is styled CM.</p>
+      <div className='DivBackground'>
+        <p id='Ptag'> This is a new CM.</p>
+      </div>
+    </div>
+  );
+
+}
+
+export default App;
