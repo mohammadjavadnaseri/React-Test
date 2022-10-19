7da5ba8220795b54407eca0611c16dfa121f2312
mohammadjavadnaseri <mjavadnaseri82@gmail.com>
2022-10-20 01:42:14 +0330

    React Test File 2

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className='ValedDiv'>
-      <p> This is styled CM.</p>
+      <p style={{color: "rgb(160, 163, 65)"}}> This is styled CM.</p>
       <div className='DivBackground'>
         <p id='Ptag'> This is a new CM.</p>
       </div>
