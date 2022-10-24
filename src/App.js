128668e5ecad4e289ab83aeaa8d617625670ec16
mohammadjavadnaseri <mjavadnaseri82@gmail.com>
2022-10-24 15:02:24 +0330

    test 3

--- src/App.js
+++ src/App.js
@@ -7,9 +7,17 @@ function App() {
       <div className='DivBackground'>
         <p id='Ptag'> This is a new CM.</p>
       </div>
+      <div className='ValedDiv'>
+        <p style={{color: "rgb(160, 163, 65)"}}> This is styled CM.</p>
+        <div className='DivBackground2'>
+          <p id='Ptag2'> This is a new CM.</p>
+        </div>
+      </div>
     </div>
   );
-
+  
 }
 
-export default App;
+
+
+export default (App);
