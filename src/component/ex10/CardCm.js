b868e1989cfccd505020c854abc29a152b5c16dd
mohammadjavadnaseri <mjavadnaseri82@gmail.com>
2022-11-24 00:10:18 +0330

    ex10

---
+++ src/component/ex10/CardCm.js
@@ -0,0 +1,50 @@
+import axios from 'axios';
+import React, { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './CardCm.css';
+
+export default function CardCm() {
+    const [showCard, cardTitle] = useState([]);
+    const [loading, loadingTime] = useState(false);
+
+    function getPosts() {
+        loadingTime(true);
+        axios({
+            method: "get",
+            url: "https://jsonplaceholder.typicode.com/posts",
+            params:{
+                _limit:50
+            }
+        }).then((response) => {
+            const responseShow = response.data.map(posts => {
+                return {
+                    userId: posts.userId,
+                    id: posts.id,
+                    title: posts.title,
+                    body:posts.body
+                };
+            });
+            cardTitle(responseShow);
+            loadingTime(false);
+        }).catch((error) => toast(error.message));
+    };
+
+
+    
+    return (
+        <>       
+            <button onClick={getPosts}>click</button>
+
+            {loading && <div className='loading'>loading...</div>}
+
+            { showCard.map(finalShow => {
+                return( <p key={finalShow.id}>{finalShow.title}</p>)
+            })}
+
+            <ToastContainer />
+
+ 
+        </>
+    )
+}
